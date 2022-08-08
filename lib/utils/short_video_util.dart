357131d231ab31d19e292cfcc90ead8866e4e8a4
lijie <lijie@hjimi.cn>
2022-08-08 18:37:14 +0800

    1、添加tiktok单个视频的下载

--- lib/utils/short_video_util.dart
+++ lib/utils/short_video_util.dart
@@ -1,19 +1,21 @@
 import '../config/constants.dart';
 
 class ShortVideoUtil {
-  static String _analyseUrl(String texts) {
-    String url = "";
-    List<String> text = texts.split(" ");
-    for (var element in text) {
-      if (element.startsWith("http") && element.contains("douyin")) {
-        url = element;
-        break;
+  static String _analyseUrl(String url) {
+    if (url.contains(_DOU_YIN)) {
+      List<String> text = url.split(" ");
+      for (var element in text) {
+        if (element.startsWith("http") && element.contains("douyin")) {
+          url = element;
+          break;
+        }
       }
     }
     return url;
   }
 
   static const String _DOU_YIN = "douyin";
+  static const String _TIK_TOK = "tiktok";
 
   // @status 0表示单个，1表示多个
   static String getRequestUrl(int status, String url, {String maxCursor = ""}) {
@@ -24,6 +26,8 @@ class ShortVideoUtil {
     if (status == 0) {
       if (url.contains(_DOU_YIN)) {
         return "${Constants.BASE_URL}/douyin/single?url=$realUrl";
+      } else if (url.contains(_TIK_TOK)) {
+        return "${Constants.BASE_URL}/tiktok/single?url=$realUrl";
       }
       return "";
     } else {
